52e220b72c67aa67e35aeff6474e3832787a08f4
chris_l <cligare@hotmail.com>
2021-06-03 14:06:37 -0700

    Listing repos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //targeting profile information
 const overview = document.querySelector(".overview");
 const username = "codercreative";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -25,4 +26,22 @@ const displayUserInfo = function (data) {
       </div> 
       `;
   overview.append(div);
+  getRepos();
+};
+
+const getRepos = async function () {
+  const fetchRepos = await fetch(
+    `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
+  );
+  const repoData = await fetchRepos.json();
+  showRepos(repoData);
+};
+
+const showRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
 };
